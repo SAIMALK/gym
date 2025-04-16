3aa98f1deb3ef556c586235ba40233041e0c8446
SAIMALK <aleesasaim496@gmail.com>
2025-04-16 17:25:34 +0500

    cc

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://bodyfuel-back.netlify.app';
 export const UPLOAD_URL = '/api/upload'
 export const USERS_URL = '/api/auth';
 
