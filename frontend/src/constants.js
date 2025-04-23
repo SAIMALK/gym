225df7ffb5c76d2565dcbde6df9ed307fb82dbae
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2025-04-23 19:01:28 +0500

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost/5000';
+export const BASE_URL = 'https://gym-bf-backend.vercel.app';
 export const UPLOAD_URL = '/api/upload'
 export const USERS_URL = '/api/auth';
 
